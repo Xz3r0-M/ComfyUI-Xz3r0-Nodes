4221597f62a241aa389a324b4c353f3f7ae80b43
Xz3r0-M <wxcsbb@outlook.com>
2026-01-27 16:07:06 +0800

    更新XStringGroup

--- xnode/xstringgroup.py
+++ xnode/xstringgroup.py
@@ -1,5 +1,5 @@
 class XStringGroup:
-    separation_method_options = ["newline", "space", "comma", "period"]
+    separation_method_options = ["none", "newline", "space", "comma", "comma_space", "period", "period_space"]
     select_string_options = ["1", "2", "3", "4", "5"]
 
     @classmethod
@@ -8,13 +8,13 @@ class XStringGroup:
             "required": {
                 "select_string": (s.select_string_options, {"default": "1", "tooltip": "Select which string to output (1-5)"}),
                 "string_1": ("STRING", {"multiline": True, "default": "", "tooltip": "First multiline string input"}),
-                "separation_method_1_2": (s.separation_method_options, {"default": "newline", "tooltip": "Separation method between String 1 and String 2"}),
+                "separation_method_1_2": (s.separation_method_options, {"default": "none", "tooltip": "Separation method between String 1 and String 2"}),
                 "string_2": ("STRING", {"multiline": True, "default": "", "tooltip": "Second multiline string input"}),
-                "separation_method_2_3": (s.separation_method_options, {"default": "newline", "tooltip": "Separation method between String 2 and String 3"}),
+                "separation_method_2_3": (s.separation_method_options, {"default": "none", "tooltip": "Separation method between String 2 and String 3"}),
                 "string_3": ("STRING", {"multiline": True, "default": "", "tooltip": "Third multiline string input"}),
-                "separation_method_3_4": (s.separation_method_options, {"default": "newline", "tooltip": "Separation method between String 3 and String 4"}),
+                "separation_method_3_4": (s.separation_method_options, {"default": "none", "tooltip": "Separation method between String 3 and String 4"}),
                 "string_4": ("STRING", {"multiline": True, "default": "", "tooltip": "Fourth multiline string input"}),
-                "separation_method_4_5": (s.separation_method_options, {"default": "newline", "tooltip": "Separation method between String 4 and String 5"}),
+                "separation_method_4_5": (s.separation_method_options, {"default": "none", "tooltip": "Separation method between String 4 and String 5"}),
                 "string_5": ("STRING", {"multiline": True, "default": "", "tooltip": "Fifth multiline string input"}),
             }
         }
@@ -28,10 +28,13 @@ class XStringGroup:
 
     def group_strings(self, select_string, string_1, string_2, string_3, string_4, string_5, separation_method_1_2, separation_method_2_3, separation_method_3_4, separation_method_4_5):
         separation_method_map = {
+            "none": "",
             "newline": "\n",
             "space": " ",
             "comma": ",",
-            "period": "."
+            "comma_space": ", ",
+            "period": ".",
+            "period_space": ". "
         }
 
         sm_1_2 = separation_method_map[separation_method_1_2]
@@ -39,19 +42,9 @@ class XStringGroup:
         sm_3_4 = separation_method_map[separation_method_3_4]
         sm_4_5 = separation_method_map[separation_method_4_5]
 
+        grouped_string = string_1 + sm_1_2 + string_2 + sm_2_3 + string_3 + sm_3_4 + string_4 + sm_4_5 + string_5
+
         strings = [string_1, string_2, string_3, string_4, string_5]
-        separation_methods = [sm_1_2, sm_2_3, sm_3_4, sm_4_5]
-
-        result_parts = []
-        for i, string in enumerate(strings):
-            if string:
-                result_parts.append(string)
-                if i < len(separation_methods) and i < len(strings) - 1:
-                    if strings[i + 1]:
-                        result_parts.append(separation_methods[i])
-
-        grouped_string = "".join(result_parts)
-
         selected_string = strings[int(select_string) - 1]
 
         return (grouped_string, selected_string, string_1, string_2, string_3, string_4, string_5)
